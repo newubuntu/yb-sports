35de00fde52747ef23f003f36c223b9eae67c387
unknown <drumtj@gmail.com>
2021-04-28 20:49:42 +0900

    origin odds

--- public/extension/main/main.js
+++ public/extension/main/main.js
@@ -677,7 +677,7 @@ async function findMatch(data){
     // data.bet365.betLink = data.bet365.betLink.replace("/dl/sportsbookredirect?", "/dl/sportsbookredirect/?");
     // console.error("betLink test", data.bet365.betLink);
 
-    data.betburger.bet365.originOdds = data.betburger.bet365.odds;
+    data.bet365.originOdds = data.bet365.odds;
 
     isCheckingMatch = true;
     if(betOption.action == "checkBetmax"){
